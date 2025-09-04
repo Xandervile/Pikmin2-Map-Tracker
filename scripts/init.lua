af1c96e510899b425d1af583dde4d709934f4937
Xander <121578706+Xandervile@users.noreply.github.com>
2025-09-04 15:23:30 +0100

    Add files via upload

---
+++ scripts/init.lua
@@ -0,0 +1,19 @@
+--- LUA Scripts
+Tracker:AddItems("items/items.json")
+
+Tracker:AddMaps("maps/maps.json")
+
+Tracker:AddLayouts("layouts/maps/valleyofrepose.json")
+Tracker:AddLayouts("layouts/maps/awakeningwood.json")
+Tracker:AddLayouts("layouts/maps/perplexingpool.json")
+Tracker:AddLayouts("layouts/maps/wistfulwild.json")
+
+Tracker:AddLayouts("layouts/maps.json")
+
+Tracker:AddLayouts("layouts/tracker.json")
+Tracker:AddLayouts("layouts/broadcast.json")
+
+Tracker:AddLocations("locations/valleyofrepose.json")
+Tracker:AddLocations("locations/awakeningwood.json")
+Tracker:AddLocations("locations/perplexingpool.json")
+Tracker:AddLocations("locations/wistfulwild.json")
